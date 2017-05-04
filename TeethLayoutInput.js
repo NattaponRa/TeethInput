6e732d5f65801bdfc34d1c843aa84525fbc2c826
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2017-05-04 22:20:56 +0700

    first commit

---
+++ TeethLayoutInput.js
@@ -0,0 +1,7 @@
+ $(function() { 
+                $(".tesseth").on("click", function(e){
+                    e.preventDefault();
+                    var alt = $(this).attr('alt');
+                    alert(alt);
+      });
+    });
---
+++ index.php
@@ -0,0 +1,122 @@
+        <!DOCTYPE html>
+        <!--
+        To change this license header, choose License Headers in Project Properties.
+        To change this template file, choose Tools | Templates
+        and open the template in the editor.
+    -->
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="jquery-1.11.0.min.js"></script>
+        <script src="jquery.imagemapster.js"></script>
+        <script src="TeethLayoutInput.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+             <link rel="stylesheet" type="text/css" href="docs.min.css">
+        <script>
+            $(function() { 
+             $('img').mapster({
+                fillColor: 'ff0000',
+                fillOpacity: 0.3
+            });
+         });
+
+     </script>
+ </head>
+ <body>
+
+<div class="bs-example" data-example-id="basic-forms">
+
+    <div class="row">
+      <div class="col-md-8">
+          <img class="img img-responsive" src="teeth-layout.jpg" alt="" usemap="#Map" />
+<map name="Map" id="Map">
+    <area style="background: red;" class="teeth" alt="1TL1" title="" href="javascript:void(0)" shape="poly" coords="933,219,956,207,989,214,1044,230,1049,263,1025,304,1002,334,980,340,956,312,936,260,930,238" />
+    <area class="teeth" alt="1TL2" title="" href="javascript:void(0)" shape="poly" coords="1056,287,1057,328,1069,359,1105,367,1140,343,1156,310,1140,268,1102,248,1069,246,1056,255" />
+    <area class="teeth" alt="1TL3" title="" href="javascript:void(0)" shape="poly" coords="1193,426,1230,420,1241,388,1241,358,1226,334,1198,320,1173,318,1152,336,1150,355,1148,374,1145,392,1144,411,1160,422" />
+    <area class="teeth" alt="1TL4" title="" href="javascript:void(0)" shape="poly" coords="1165,476,1164,506,1190,528,1236,535,1262,527,1292,510,1301,484,1292,454,1262,438,1228,432,1204,439" />
+    <area class="teeth" alt="1TL5" title="" href="javascript:void(0)" shape="poly" coords="1210,589,1217,617,1254,631,1289,632,1326,611,1338,584,1329,547,1304,533,1269,532,1238,541" />
+    <area class="teeth" alt="1TL6" title="" href="javascript:void(0)" shape="poly" coords="1232,663,1216,697,1214,731,1229,748,1237,775,1266,795,1297,792,1338,784,1365,759,1369,731,1370,685,1360,651,1329,635,1288,637" />
+    <area class="teeth" alt="1TL7" title="" href="javascript:void(0)" shape="poly" coords="1226,885,1254,917,1302,924,1352,915,1377,888,1388,843,1378,803,1342,789,1308,795,1284,801,1269,811,1250,819,1233,840" />
+    <area class="teeth" alt="1TL8" title="" href="javascript:void(0)" shape="poly" coords="1244,1010,1261,1031,1294,1039,1333,1046,1368,1036,1389,1018,1394,990,1392,956,1372,931,1341,926,1306,928,1282,944,1257,963,1246,986" />
+    <area class="teeth" alt="1BL8" title="" href="javascript:void(0)" shape="poly" coords="1253,1328,1278,1357,1316,1372,1366,1363,1385,1321,1384,1276,1369,1244,1338,1224,1297,1220,1273,1248,1254,1288" />
+    <area class="teeth" alt="1BL7" title="" href="javascript:void(0)" shape="poly" coords="1342,1402,1306,1376,1249,1390,1220,1431,1210,1476,1217,1515,1257,1527,1296,1528,1324,1533,1341,1505,1352,1456,1346,1416" />
+    <area class="teeth" alt="1BL6" title="" href="javascript:void(0)" shape="poly" coords="1194,1612,1178,1621,1174,1641,1177,1667,1197,1692,1236,1707,1282,1700,1310,1676,1321,1643,1316,1620,1325,1584,1312,1545,1281,1533,1244,1533,1221,1537,1198,1553,1190,1588" />
+    <area class="teeth" alt="1BL5" title="" href="javascript:void(0)" shape="poly" coords="1148,1765,1189,1816,1226,1812,1262,1788,1272,1763,1253,1724,1225,1707,1198,1704,1177,1704,1160,1711,1146,1717" />
+    <area class="teeth" alt="1BL4" title="" href="javascript:void(0)" shape="poly" coords="1201,1824,1212,1858,1209,1879,1196,1898,1177,1916,1156,1922,1133,1908,1117,1875,1108,1851,1118,1828,1133,1819" />
+    <area class="teeth" alt="1BL3" title="" href="javascript:void(0)" shape="poly" coords="1156,1940,1154,1967,1146,2000,1116,2015,1077,2015,1060,1993,1060,1953,1072,1920,1086,1911,1104,1917,1121,1921,1141,1921" />
+    <area class="teeth" alt="1BL2" title="" href="javascript:void(0)" shape="poly" coords="1022,1947,1049,1982,1073,2023,1053,2042,1020,2050,998,2043,985,2027,993,1988,1005,1950" />
+    <area class="teeth" alt="1BL1" title="" href="javascript:void(0)" shape="poly" coords="950,1972,965,2000,978,2019,980,2041,966,2056,950,2060,930,2060,910,2052,900,2032,902,1999,917,1980,936,1968,950,1961" />
+    <area class="teeth" alt="1BR1" title="" href="javascript:void(0)" shape="poly" coords="862,1964,878,1979,894,1993,896,2009,896,2039,892,2053,881,2063,853,2063,824,2051,817,2029,825,2009,850,1971" />
+    <area class="teeth" alt="1BR2" title="" href="javascript:void(0)" shape="poly" coords="798,1964,804,1992,816,2012,816,2032,808,2040,782,2047,757,2045,738,2033,725,2008,736,1983,752,1964,770,1949,790,1949" />
+    <area class="teeth" alt="1BR3" title="" href="javascript:void(0)" shape="poly" coords="738,1937,732,1972,720,1999,689,2004,664,1993,646,1969,633,1944,630,1925,653,1909,685,1908,709,1911,729,1921" />
+    <area class="teeth" alt="1BR4" title="" href="javascript:void(0)" shape="poly" coords="640,1804,666,1815,686,1832,681,1864,665,1891,640,1908,606,1903,586,1887,572,1852,582,1820,608,1806" />
+    <area class="teeth" alt="1BR5" title="" href="javascript:void(0)" shape="poly" coords="625,1700,640,1728,638,1771,628,1795,605,1799,568,1803,545,1788,534,1767,525,1739,534,1715,552,1698,569,1692,601,1691" />
+    <area class="teeth" alt="1BR6" title="" href="javascript:void(0)" shape="poly" coords="591,1680,619,1632,615,1594,603,1586,597,1542,561,1518,527,1518,491,1530,475,1564,479,1600,485,1612,487,1644,501,1674,519,1682,551,1688" />
+    <area class="teeth" alt="1BR7" title="" href="javascript:void(0)" shape="poly" coords="565,1452,573,1406,553,1370,513,1356,473,1362,445,1390,441,1432,445,1458,457,1488,473,1504,497,1516,523,1516,555,1512,569,1506" />
+    <area class="teeth" alt="1BR8" title="" href="javascript:void(0)" shape="poly" coords="533,1275,525,1228,482,1201,445,1207,422,1243,420,1293,436,1341,476,1356,518,1345,540,1336,542,1297" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="524,1011,552,971,546,930,504,907,460,906,426,926,421,955,424,983,433,1008,452,1027,469,1032,496,1028" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="556,898,578,868,573,827,553,795,513,779,470,774,445,790,434,826,446,874,481,895,518,900" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="561,771,594,734,597,680,564,640,513,622,470,642,461,674,461,703,469,739,482,758,505,771" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="610,604,616,579,604,554,590,531,569,518,542,516,509,527,490,546,488,575,494,600,517,614,554,626,586,622" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="633,519,653,503,660,480,645,447,617,424,581,419,538,439,529,471,541,496,561,515,590,524,610,528" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="690,396,684,359,682,315,678,302,654,304,624,314,604,319,590,340,594,366,598,394,618,414,645,414,662,410,678,406" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="785,344,790,299,774,259,753,243,716,255,692,268,684,295,690,327,720,343,754,368,769,367" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="893,283,918,240,912,212,864,210,824,215,794,227,776,250,793,284,810,312,830,331,850,332" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2564,900,2584,923,2598,944,2625,942,2645,922,2665,900,2656,880,2638,864,2613,859,2592,863,2580,872,2566,875,2562,884" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2674,920,2690,946,2694,964,2729,971,2752,963,2750,924,2745,902,2722,891,2701,896,2680,902,2672,908" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2754,992,2754,1023,2768,1046,2797,1048,2822,1048,2844,1034,2837,1006,2840,983,2829,967,2802,963,2774,959,2760,963" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2796,1080,2774,1099,2765,1131,2776,1159,2800,1181,2838,1184,2874,1175,2900,1156,2916,1137,2925,1109,2920,1081,2908,1063,2882,1053,2849,1052,2829,1052,2825,1056" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2790,1252,2793,1288,2786,1311,2793,1348,2842,1361,2881,1357,2920,1344,2933,1321,2953,1304,2965,1264,2965,1223,2950,1193,2926,1179,2898,1175,2869,1177,2840,1189,2821,1207,2806,1225" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2800,1604,2793,1647,2794,1698,2810,1735,2830,1758,2862,1766,2893,1760,2924,1738,2945,1699,2949,1662,2937,1618,2916,1602,2902,1579,2861,1572,2830,1576,2808,1592,2805,1599" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2772,1824,2773,1856,2786,1892,2828,1907,2868,1895,2877,1860,2877,1820,2865,1779,2840,1760,2817,1760,2797,1777,2782,1791" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2708,1936,2702,1967,2712,1987,2746,1989,2773,1977,2784,1945,2789,1927,2772,1911,2745,1912,2733,1916,2725,1919" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2701,2011,2686,2033,2665,2047,2646,2051,2633,2039,2629,2024,2645,2001,2666,1988,2680,1984,2694,1984,2701,1989,2701,1991" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2548,2056,2578,2068,2605,2059,2621,2049,2606,2019,2590,2012,2564,2011,2545,2025,2542,2040" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2445,2030,2454,2055,2481,2063,2502,2056,2524,2050,2524,2031,2506,2008,2482,2004,2466,2016" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2369,2032,2396,2040,2424,2040,2433,2031,2441,2019,2424,1998,2404,1978,2380,1975,2360,1983,2360,2004,2362,2020" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2350,1987,2349,1956,2342,1916,2317,1892,2288,1885,2265,1888,2261,1908,2262,1933,2265,1964,2286,1979,2306,1985,2340,1993" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2230,1886,2258,1879,2276,1882,2289,1859,2289,1811,2281,1762,2246,1735,2198,1740,2180,1770,2177,1804,2185,1828,2174,1844,2174,1867,2189,1879,2208,1879" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2261,1695,2258,1641,2250,1604,2234,1559,2176,1555,2130,1580,2117,1619,2122,1648,2124,1671,2134,1713,2164,1732,2198,1736,2228,1724,2252,1711,2257,1697" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2285,1324,2292,1259,2268,1208,2233,1170,2177,1164,2129,1190,2114,1220,2125,1248,2125,1287,2156,1312,2190,1323,2218,1340,2234,1346,2261,1340,2252,1340" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2296,1156,2304,1113,2296,1080,2266,1057,2222,1045,2189,1039,2170,1055,2169,1096,2181,1136,2212,1157,2245,1165,2270,1164" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2333,1022,2344,990,2337,962,2313,951,2289,944,2262,954,2250,976,2250,1010,2265,1035,2298,1043,2324,1039" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2377,968,2406,962,2418,944,2430,926,2436,908,2412,888,2374,886,2353,896,2338,915,2340,938,2346,963,2364,970" />
+    <area class="teeth" alt="" title="" href="javascript:void(0)" shape="poly" coords="2496,936,2508,924,2525,910,2538,896,2534,875,2513,856,2481,859,2454,867,2434,883,2440,902,2450,914,2462,938,2474,938" />
+
+</map>
+      </div>
+    <div class="col-md-4">
+         <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputFile">File input</label>
+            <input type="file" id="exampleInputFile">
+            <p class="help-block">Example block-level help text here.</p>
+        </div>
+        <div class="checkbox">
+            <label>
+              <input type="checkbox"> Check me out
+          </label>
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+</div>
+
+ 
+
+ </div>
+
+
+
+</body>
+</html>
